858dfe673e6a75e7445f65ba34d52554ce235da5
Bisma Ahmed <bahmed0@chicagobooth.edu>
2017-07-26 20:56:31 +0000

    add new

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,17 +8,22 @@ class MoviesController < ApplicationController
   end
 
   def new_form
+    
+    # render "movies/new_form.html.erb"
   end
 
   def create_row
-    @movie.title = params[:the_title]
-    @movie.year = params[:the_year]
-    @movie.duration = params[:the_duration]
-    @movie.description = params[:the_description]
-    @movie.image_url = params[:the_image_url]
-    @movie.director_id = params[:the_director_id]
-
+    
+    @movie = Movie.new 
+    @movie.title = params["title"]
+    @movie.year = params["year"]
+    @movie.duration = params["duration"]
+    @movie.description = params["description"]
+    @movie.image_url = params["image_url"]
+    @movie.director_id = params["director_id"]
     @movie.save
+    
+    # @movie = Movie.find(params["id"])
 
     render("show")
   end
